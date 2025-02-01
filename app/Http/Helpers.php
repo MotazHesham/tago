8af8c291a4e838f662897f0160b5ec071af57592
Motaz Hesham <motazhesham01@gmail.com>
2025-02-01 21:07:25 +0200

    edit random string

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -39,7 +39,7 @@ if (!function_exists('partition')) {
 
 if (!function_exists('generateRandomString')) {
     function generateRandomString($length = 10) {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ=#%$@&';
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $charactersLength = strlen($characters);
         $randomString = '';
         for ($i = 0; $i < $length; $i++) {
