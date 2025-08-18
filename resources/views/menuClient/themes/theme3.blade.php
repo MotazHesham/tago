ac9eded6384838ac27dfd1d4d5d66cf816fb10d5
Motaz Hesham <motazhesham01@gmail.com>
2025-08-18 20:36:06 +0300

    new theme

---
+++ resources/views/menuClient/themes/theme3.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />  
+</head>
+
+<body> 
+
+    @foreach($menuClientList->categories as $category)
+        <img src="{{ $category->banner ? $category->banner->getUrl() : '' }}" alt="">
+    @endforeach 
+</body>
+
+</html>
